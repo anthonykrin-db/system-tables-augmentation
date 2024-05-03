60b31e39e944e46d451546719de822b340a962a6
anthony.krinsky@databricks.com <anthony.krinsky@databricks.com>
2024-05-03 04:35:42 +0000

    Reordered 03-ExtractDims, improved error handling on dashboards

--- notebooks/03-ExtractDims.py
+++ notebooks/03-ExtractDims.py
@@ -136,6 +136,152 @@ spark.sql("SET spark.databricks.delta.properties.defaults.columnMapping.mode = n
 
 # COMMAND ----------
 
+# DBTITLE 1,Delta Pipelines
+import json
+import pandas as pd
+import requests
+
+response = requests.get(DLT_PIPELINES_URL, headers=AUTH_HEADER)
+
+if response.status_code != 200:
+  raise Exception(response.text)
+
+response_json = response.json()
+#print(response_json)
+
+data=[]
+while response_json["statuses"]:
+  data.append(response_json["statuses"])
+
+  next_page_token = None
+  if "next_page_token" in response_json:
+    next_page_token=response_json["next_page_token"]
+    url=f'{DLT_PIPELINES_URL}?page_token={next_page_token}'
+    #print("Calling: {}".format(url))
+    response = requests.get(url, headers=AUTH_HEADER)
+    #print(response)
+    response_json = response.json()
+  else:
+    break
+
+if len(data)>0:
+  combined_df = json_documents_combined_panda(data,[],["latest_updates"])
+  dump_pandas_info(combined_df)
+  # print("parsed_json: {}".format(parsed_json))
+  dlt_pipelines = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
+
+  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
+  dlt_pipelines.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + DLT_PIPELINES_TABLE_NAME)
+else:
+  print("No data")
+
+
+
+# COMMAND ----------
+
+# DBTITLE 1,clusters
+import json
+import pandas as pd
+
+response = requests.get(CLUSTERS_URL, headers=AUTH_HEADER)
+
+if response.status_code != 200:
+  raise Exception(response.text)
+response_json = response.json()
+
+
+if "clusters" in response_json:
+  combined_df = json_documents_combined_panda(response_json["clusters"],["azure_attributes"])
+  dump_pandas_info(combined_df)
+  # print("parsed_json: {}".format(parsed_json))
+  clusters = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
+
+  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
+  clusters.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + CLUSTERS_TABLE_NAME)
+
+# COMMAND ----------
+
+# DBTITLE 1,intance pools
+import json
+import pandas as pd
+
+response = requests.get(INSTANCE_POOLS_URL, headers=AUTH_HEADER)
+
+if response.status_code != 200:
+  raise Exception(response.text)
+response_json = response.json()
+#print(response_json)
+if "instance_pools" in response_json:
+  combined_df = json_documents_combined_panda(response_json["instance_pools"])
+  dump_pandas_info(combined_df)
+  pools = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
+  ##print("Saving table: {}.{}".format(DATABASE_NAME, WORKSPACE_OBJECTS_TABLE_NAME))
+  pools.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + INSTANCE_POOLS_TABLE_NAME)
+
+# COMMAND ----------
+
+# DBTITLE 1,Warehouses
+import json
+import pandas as pd
+
+response = requests.get(WAREHOUSES_URL, headers=AUTH_HEADER)
+
+if response.status_code != 200:
+  raise Exception(response.text)
+response_json = response.json()
+# print(response_json)
+
+if "warehouses" in response_json:
+  combined_df = json_documents_combined_panda(response_json["warehouses"])
+  dump_pandas_info(combined_df)
+  whs = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
+
+  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
+  whs.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + WAREHOUSES_TABLE_NAME)
+
+# COMMAND ----------
+
+# DBTITLE 1,Dashboards
+
+## there is no next page token with this API so will need to iterate thorough all results until we get empty results
+
+base_url = f'{DASHBOARDS_URL}/admin?page_size={PAGE_SIZE}'
+empty_response = {'message': 'Page is out of range.'}
+
+data = []
+# initial request to set up the objects
+res = get_page_result(base_url, 1, AUTH_HEADER)
+responses_json = res.json()
+if "results" in responses_json and responses_json["results"]:
+    #print(responses_json["results"])
+    data.append(parse_doc(responses_json["results"]))
+# print(f'PAGE 1 done')
+    page = 2
+    while responses_json != empty_response and responses_json["count"]>0:
+        res = get_page_result(base_url, page, AUTH_HEADER)#
+        responses_json = res.json()
+        if "results" in responses_json and responses_json["results"]:
+          #print(responses_json["results"])
+          data.append(parse_doc(responses_json["results"]))
+        #print(f'PAGE {page} done')
+        page += 1
+
+if len(data)>0:
+  combined_df = json_documents_combined_panda(data,["options"])
+
+  dump_pandas_info(combined_df)
+
+  # print("parsed_json: {}".format(parsed_json))
+  dashs = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
+
+  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
+  dashs.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + DASHBOARDS_TABLE_NAME)
+else:
+  print("No data")
+
+
+# COMMAND ----------
+
 # DBTITLE 1,notebooks
 import json
 import pandas as pd
@@ -193,152 +339,3 @@ notebooks = spark.createDataFrame(combined_df).withColumn("snapshot_time", curre
 
 ##print("Saving table: {}.{}".format(DATABASE_NAME, WORKSPACE_OBJECTS_TABLE_NAME))
 notebooks.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + WORKSPACE_OBJECTS_TABLE_NAME)
-
-# COMMAND ----------
-
-# DBTITLE 1,Delta Pipelines
-import json
-import pandas as pd
-import requests
-
-response = requests.get(DLT_PIPELINES_URL, headers=AUTH_HEADER)
-
-if response.status_code != 200:
-  raise Exception(response.text)
-
-response_json = response.json()
-#print(response_json)
-
-data=[]
-while response_json["statuses"]:
-  data.append(response_json["statuses"])
-
-  next_page_token = None
-  if "next_page_token" in response_json:
-    next_page_token=response_json["next_page_token"]
-    url=f'{DLT_PIPELINES_URL}?page_token={next_page_token}'
-    #print("Calling: {}".format(url))
-    response = requests.get(url, headers=AUTH_HEADER)
-    #print(response)
-    response_json = response.json()
-  else:
-    break
-
-if len(data)>0:
-  combined_df = json_documents_combined_panda(data,[],["latest_updates"])
-  dump_pandas_info(combined_df)
-  # print("parsed_json: {}".format(parsed_json))
-  dlt_pipelines = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
-
-  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
-  dlt_pipelines.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + DLT_PIPELINES_TABLE_NAME)
-else:
-  print("No data")
-
-
-
-# COMMAND ----------
-
-# DBTITLE 1,Dashboards
-## TODO: IMPROVE ERROR HANDLING
-
-
-## there is no next page token with this API so will need to iterate thorough all results until we get empty results
-
-base_url = f'{DASHBOARDS_URL}/admin?page_size={PAGE_SIZE}'
-empty_response = {'message': 'Page is out of range.'}
-
-# initial request to set up the objects
-res = get_page_result(base_url, 1, AUTH_HEADER)
-data = [result_to_json(res)]
-# print(f'PAGE 1 done')
-
-# from now, start at page 2 and incrememnt up in while loop
-page = 2
-
-#data.append(parse_doc(res))
-responses_json = res.json()
-#print(responses_json)
-
-while responses_json != empty_response and responses_json["count"]>0:
-    res = get_page_result(base_url, page, AUTH_HEADER)#
-    responses_json = res.json()
-    if "results" in responses_json and responses_json["results"]:
-      #print(responses_json["results"])
-      data.append(parse_doc(responses_json["results"]))
-    #print(f'PAGE {page} done')
-    page += 1
-
-combined_df = json_documents_combined_panda(data,["options"])
-
-dump_pandas_info(combined_df)
-
-# print("parsed_json: {}".format(parsed_json))
-dashs = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
-
-##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
-dashs.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + DASHBOARDS_TABLE_NAME)
-
-
-
-# COMMAND ----------
-
-# DBTITLE 1,intance pools
-import json
-import pandas as pd
-
-response = requests.get(INSTANCE_POOLS_URL, headers=AUTH_HEADER)
-
-if response.status_code != 200:
-  raise Exception(response.text)
-response_json = response.json()
-#print(response_json)
-if "instance_pools" in response_json:
-  combined_df = json_documents_combined_panda(response_json["instance_pools"])
-  dump_pandas_info(combined_df)
-  pools = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
-  ##print("Saving table: {}.{}".format(DATABASE_NAME, WORKSPACE_OBJECTS_TABLE_NAME))
-  pools.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + INSTANCE_POOLS_TABLE_NAME)
-
-# COMMAND ----------
-
-# DBTITLE 1,clusters
-import json
-import pandas as pd
-
-response = requests.get(CLUSTERS_URL, headers=AUTH_HEADER)
-
-if response.status_code != 200:
-  raise Exception(response.text)
-response_json = response.json()
-
-
-if "clusters" in response_json:
-  combined_df = json_documents_combined_panda(response_json["clusters"],["azure_attributes"])
-  dump_pandas_info(combined_df)
-  # print("parsed_json: {}".format(parsed_json))
-  clusters = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
-
-  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
-  clusters.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + CLUSTERS_TABLE_NAME)
-
-# COMMAND ----------
-
-# DBTITLE 1,Warehouses
-import json
-import pandas as pd
-
-response = requests.get(WAREHOUSES_URL, headers=AUTH_HEADER)
-
-if response.status_code != 200:
-  raise Exception(response.text)
-response_json = response.json()
-# print(response_json)
-
-if "warehouses" in response_json:
-  combined_df = json_documents_combined_panda(response_json["warehouses"])
-  dump_pandas_info(combined_df)
-  whs = spark.createDataFrame(combined_df).withColumn("snapshot_time", current_timestamp())
-
-  ##print("Saving table: {}.{}".format(DATABASE_NAME, CLUSTERS_TABLE_NAME))
-  whs.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable(DATABASE_NAME + "." + WAREHOUSES_TABLE_NAME)
