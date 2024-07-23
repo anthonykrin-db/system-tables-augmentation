3cfb70aeda2224b1cbfefe57dd97ca89b0d19437
anthony.krinsky@databricks.com <anthony.krinsky@databricks.com>
2024-07-23 04:59:05 +0000

    Updated SQL parser
    Parses 1,000 sql at one time.

--- notebooks/06-SQL-Parsing.py
+++ notebooks/06-SQL-Parsing.py
@@ -20,40 +20,14 @@
 
 from py4j.java_gateway import java_import
 from pyspark.sql.functions import udf, col
-from pyspark.sql.types import ArrayType, StringType
+from datetime import datetime
+from pyspark.sql.types import ArrayType, StructType, StructField, StringType, DateType,TimestampType
 
 java_import(spark._jvm, "parser.ColumnToken") 
 java_import(spark._jvm, "parser.ParseResult") 
 java_import(spark._jvm, "parser.SqlStatementParser")
 
 parser = spark._jvm.SqlStatementParser()
-
-# COMMAND ----------
-
-# DBTITLE 1,Parsing Test
-
-
-sql = "with y as (SELECT * FROM x.tbl_a) select a,b,c,DATETOSTRING(date_attr_name,'mm/dd/yyyy') from y"
-
-result = parser.parseTokens(sql)
-
-if result.isSuccess() is True:
-
-  print("Success: " + str(result.isSuccess())) 
-  for token in result.getTokens(): 
-    print("Catalog: " + str(token.getCatalog())) 
-    print("Schema: " + str(token.getSchema())) 
-    print("Table: " + str(token.getTable())) 
-    print("Column: " + str(token.getColumn())) 
-    print("-------------\n")
-
-else: print("Error: " + result.getErrMsg())
-
-# COMMAND ----------
-
-# DBTITLE 1,Parse query history
-from datetime import datetime
-from pyspark.sql.types import StructType, StructField, StringType, DateType,TimestampType
 
 # Assuming a workaround or placeholder for the SQL parsing logic
 def parse_sql(sql):
@@ -73,8 +47,62 @@ def parse_sql(sql):
         print(f"Exception parsing {sql}: {e}")
     return tokens
 
+def parse_sql_statements(sqlList):
+    results = []
+    if len(sqlList)==0:
+        return results
+    try:
+        multipleResults = parser.parseTokens(sqlList)
+        for result in multipleResults:
+            if result.isSuccess():
+                #print("Success: " + str(result.isSuccess()))
+                results.append(result.getTokens())
+            else:
+                print(f"Unable to parse: {result.getErrMsg()}")
+    except Exception as e:
+        print(f"Exception parsing {sql}: {e}")
+    return results
 
-limit = 100
+def printToken(token):
+    print("===== TOKEN =====\n")
+    print("Catalog: " + str(token.getCatalog())) 
+    print("Schema: " + str(token.getSchema())) 
+    print("Table: " + str(token.getTable())) 
+    print("Column: " + str(token.getColumn())) 
+    print("-------------\n")
+
+# COMMAND ----------
+
+# DBTITLE 1,Parsing Tests
+# MAGIC %md
+# MAGIC
+# MAGIC
+# MAGIC sql = "with y as (SELECT * FROM x.tbl_a) select a,b,c,DATETOSTRING(date_attr_name,'mm/dd/yyyy') from y"
+# MAGIC
+# MAGIC tokens = parse_sql(sql)
+# MAGIC for token in tokens: 
+# MAGIC   printToken(token)
+# MAGIC
+# MAGIC print("**************** Multiple ****************")
+# MAGIC
+# MAGIC sqlList = []
+# MAGIC sqlList.append(sql)
+# MAGIC sqlList.append(sql)
+# MAGIC sqlList.append(sql)
+# MAGIC sqlList.append(sql)
+# MAGIC results = parse_sql_statements(sqlList)
+# MAGIC
+# MAGIC for result in results:
+# MAGIC   for token in result:
+# MAGIC     printToken(token)
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# DBTITLE 1,Parse query history
+
+limit = 1000
 complete = False
 
 # SQL_COLUMNS_TABLE_NAME
@@ -86,7 +114,6 @@ query_history_start_time = lookup_last_record_value(
 target_table_path = f"{DATABASE_NAME}.{SQL_COLUMNS_TABLE_NAME}"
 
 original_statement_sql = f"SELECT statement_id, executed_by, start_time, end_time, statement_text FROM finops.system_query.history where statement_type = 'SELECT' AND 1=1 ORDER BY start_time ASC LIMIT {limit}"
-
 
 # Create a schema for the parsed statement rows
 parsed_sql_schema = StructType(
@@ -118,30 +145,51 @@ while complete is False:
 
     query_statement_rows = query_statements_df.collect()
 
-    parsed_statement_rows = []
+    sql_statements = []
+    statement_metadata = []
+
     for row in query_statement_rows:
-        statement_id = row["statement_id"]
-        start_time = row["start_time"]
-        end_time = row["end_time"]
-        statement_text = row["statement_text"]
-        executed_by = row["executed_by"]
-        tokens = parse_sql(statement_text)
+        
+        sql_statements.append(statement_text)
+        statement_metadata.append({
+            "statement_id": row["statement_id"],
+            "executed_by": row["executed_by"],
+            "start_time": row["start_time"],
+            "end_time": row["end_time"],
+            "statement_text": row["statement_text"]
+        })
+
+        # Update query_history_start_time
+        query_history_start_time = start_time
+
+    # Parse all SQL statements at once
+    all_tokens = parse_sql_statements(sql_statements)
+
+    parsed_statement_rows = []
+    for tokens, metadata in zip(all_tokens, statement_metadata):
         for token in tokens:
             catalog = str(token.getCatalog())
             schema = str(token.getSchema())
             table = str(token.getTable())
             column = str(token.getColumn())
-            # Create a row from statement_id, start_time, end_time, statement_text, schema, table, column
-            parsed_row = (statement_id, executed_by,start_time, end_time, catalog, schema, table, column)
-            # Add to parsed_statement_rows
+            
+            parsed_row = (
+                metadata["statement_id"],
+                metadata["executed_by"],
+                metadata["start_time"],
+                metadata["end_time"],
+                catalog,
+                schema,
+                table,
+                column
+            )
+            
             parsed_statement_rows.append(parsed_row)
-
-        # increment start time
-        query_history_start_time = start_time
 
     # Create a DataFrame from parsed_statement_rows
     parsed_df = spark.createDataFrame(parsed_statement_rows, parsed_sql_schema)
-    parsed_df.show()
+    #parsed_df.show()
     if parsed_df.count()>0:
         parsed_df.dropDuplicates().write.format("delta").option("mergeSchema", "true").mode("append").saveAsTable(target_table_path)
+        print(f"Committed {parsed_df.count()} records.")
 
