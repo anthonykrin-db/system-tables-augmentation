6ee55e0bcff62120611e7f696f6d333d509c9718
anthony.krinsky@databricks.com <anthony.krinsky@databricks.com>
2024-07-24 16:36:34 +0000

    Updated gudu jar, parseMultipleStatements

--- notebooks/06-SQL-Parsing.py
+++ notebooks/06-SQL-Parsing.py
@@ -13,7 +13,7 @@
 # MAGIC
 # MAGIC ```
 # MAGIC /Volumes/finops/system_lookups/libs/gudusoft.gsqlparser-2.8.5.8.jar
-# MAGIC /Volumes/finops/system_lookups/libs/sql_parser_excl_gudu_v1.0.jar```
+# MAGIC /Volumes/finops/system_lookups/libs/sql_parser_excl_gudu_v1.3.jar```
 # MAGIC
 
 # COMMAND ----------
@@ -42,7 +42,7 @@ def parse_sql(sql):
                 # Placeholder parsing logic
                 tokens.append(token)  # Simplified example
         else:
-            print(f"Unable to parse: {result}")
+            print(f"Unable to parse {sql}: {result}")
     except Exception as e:
         print(f"Exception parsing {sql}: {e}")
     return tokens
@@ -52,13 +52,14 @@ def parse_sql_statements(sqlList):
     if len(sqlList)==0:
         return results
     try:
-        multipleResults = parser.parseTokens(sqlList)
+        multipleResults = parser.parseMultipleTokens(sqlList)
         for result in multipleResults:
             if result.isSuccess():
                 #print("Success: " + str(result.isSuccess()))
                 results.append(result.getTokens())
             else:
-                print(f"Unable to parse: {result.getErrMsg()}")
+                print(f"Unable to parse {sql}: {result.getErrMsg()}")
+                continue
     except Exception as e:
         print(f"Exception parsing {sql}: {e}")
     return results
@@ -74,35 +75,40 @@ def printToken(token):
 # COMMAND ----------
 
 # DBTITLE 1,Parsing Tests
-# MAGIC %md
-# MAGIC
-# MAGIC
-# MAGIC sql = "with y as (SELECT * FROM x.tbl_a) select a,b,c,DATETOSTRING(date_attr_name,'mm/dd/yyyy') from y"
-# MAGIC
-# MAGIC tokens = parse_sql(sql)
-# MAGIC for token in tokens: 
-# MAGIC   printToken(token)
-# MAGIC
-# MAGIC print("**************** Multiple ****************")
-# MAGIC
-# MAGIC sqlList = []
-# MAGIC sqlList.append(sql)
-# MAGIC sqlList.append(sql)
-# MAGIC sqlList.append(sql)
-# MAGIC sqlList.append(sql)
-# MAGIC results = parse_sql_statements(sqlList)
-# MAGIC
-# MAGIC for result in results:
-# MAGIC   for token in result:
-# MAGIC     printToken(token)
-# MAGIC
-# MAGIC
+
+
+sql = "with y as (SELECT * FROM x.tbl_a) select a,b,c,DATETOSTRING(date_attr_name,'mm/dd/yyyy') from y"
+sql = "select cs.DeviceId,cs.MessageDateTime,cs.HeartRate, cs.RespiratoryRate,cs.BloodPressureSystolic, cs.BloodPressureDiastolic,concat_ws('\\\\',string(int(cs.BloodPressureSystolic)),string(int(cs.BloodPressureDiastolic))) as BloodPressure, cs.BodyTemperature, cs.HeartRateVariability from iomt_demo.charts_silver cs where cs.DeviceId = '1qsi9p8t5l2'  and cs.HeartRate is not null  and cs.MessageDateHour = date_format(current_timestamp(), 'yyyy-MM-dd HH:00:00') order by cs.MessageDateTime desc LIMIT 1000"
+
+tokens = parse_sql(sql)
+for token in tokens: 
+  printToken(token)
+
+print("**************** Multiple ****************")
+
+sqlList = []
+sqlList.append(sql)
+sqlList.append(sql)
+sqlList.append(sql)
+sqlList.append(sql)
+results = parse_sql_statements(sqlList)
+
+for result in results:
+  for token in result:
+    printToken(token)
+
+
 
 # COMMAND ----------
 
 # DBTITLE 1,Parse query history
 
-limit = 1000
+import time
+import datetime
+
+start_time = time.time()
+
+statement_batch_size = 1000
 complete = False
 
 # SQL_COLUMNS_TABLE_NAME
@@ -113,7 +119,7 @@ query_history_start_time = lookup_last_record_value(
 # Incremental record check
 target_table_path = f"{DATABASE_NAME}.{SQL_COLUMNS_TABLE_NAME}"
 
-original_statement_sql = f"SELECT statement_id, executed_by, start_time, end_time, statement_text FROM finops.system_query.history where statement_type = 'SELECT' AND 1=1 ORDER BY start_time ASC LIMIT {limit}"
+original_statement_sql = f"SELECT statement_id, executed_by, start_time, end_time, statement_text FROM finops.system_query.history where statement_type = 'SELECT' AND 1=1 ORDER BY start_time ASC LIMIT {statement_batch_size}"
 
 # Create a schema for the parsed statement rows
 parsed_sql_schema = StructType(
@@ -132,7 +138,10 @@ parsed_sql_schema = StructType(
 if query_history_start_time is None:
     query_history_start_time = datetime.strptime("1970-01-01", "%Y-%m-%d")
 
+batch = 0
 while complete is False:
+    loop_start = time.time()
+    batch = batch +1
     query_statement_sql = original_statement_sql.replace(
         "1=1", f" start_time>'{query_history_start_time}'"
     )
@@ -150,6 +159,8 @@ while complete is False:
 
     for row in query_statement_rows:
         
+        sql = row["statement_text"]
+        print(f"Parsing sql: {sql}")
         sql_statements.append(row["statement_text"])
         statement_metadata.append({
             "statement_id": row["statement_id"],
@@ -191,5 +202,10 @@ while complete is False:
     #parsed_df.show()
     if parsed_df.count()>0:
         parsed_df.dropDuplicates().write.format("delta").option("mergeSchema", "true").mode("append").saveAsTable(target_table_path)
-        print(f"Committed {parsed_df.count()} records.")
+        loop_end = time.time()
+        loop_duration = loop_end - loop_start
+        total_elapsed = loop_end - start_time
+        print(f"Batch {batch} duration: {loop_duration:.2f} seconds. Total time elapsed: {total_elapsed:.2f} seconds")
+        print(f"Parsed {statement_batch_size} SQL statements.  Committed {parsed_df.count()} parsed cat/schema/table/column records.  Timestamp: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+   
 
